8206b675781f2d5792d8855e90bae3b29c17f8b3
Pedro Henrique GOdri <pedrogodri@gmail.com>
2023-03-23 20:47:06 -0300

    Auto stash before rebase of "refs/heads/main"

--- produtos/src/main/java/br/com/api/produtos/controle/ProdutoControle.java
+++ produtos/src/main/java/br/com/api/produtos/controle/ProdutoControle.java
@@ -1,7 +1,10 @@
 package br.com.api.produtos.controle;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.api.produtos.modelo.ProdutoModelo;
--- produtos/src/main/java/br/com/api/produtos/servico/ProdutoServico.java
+++ produtos/src/main/java/br/com/api/produtos/servico/ProdutoServico.java
@@ -2,7 +2,6 @@ package br.com.api.produtos.servico;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import br.com.api.produtos.modelo.ProdutoModelo;
 import br.com.api.produtos.repositorio.IProdutoRepositorio;
 
