438fdc8c06f3c00d4d26880e698efb270f7649b3
Pedro Henrique GOdri <pedrogodri@gmail.com>
2023-03-23 20:56:22 -0300

    feat: Rota para cadastrar produtos

--- produtos/src/main/java/br/com/api/produtos/controle/ProdutoControle.java
+++ produtos/src/main/java/br/com/api/produtos/controle/ProdutoControle.java
@@ -1,7 +1,10 @@
 package br.com.api.produtos.controle;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.api.produtos.modelo.ProdutoModelo;
@@ -12,6 +15,11 @@ public class ProdutoControle {
     
     @Autowired
     private ProdutoServico produtoServico;
+
+    @PostMapping("/cadastrar")
+    public ResponseEntity<?> cadastrar(@RequestBody ProdutoModelo produtoModelo) {
+        return produtoServico.cadastrar(produtoModelo);
+    }
 
     @GetMapping("/listar")
     public Iterable<ProdutoModelo> listar() {
--- produtos/src/main/java/br/com/api/produtos/servico/ProdutoServico.java
+++ produtos/src/main/java/br/com/api/produtos/servico/ProdutoServico.java
@@ -1,9 +1,8 @@
 package br.com.api.produtos.servico;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
