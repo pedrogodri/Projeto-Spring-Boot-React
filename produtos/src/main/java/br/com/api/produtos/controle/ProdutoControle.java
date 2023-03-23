6991d2452f19d40b85adcd518c3d458b4f3455c4
Pedro Henrique GOdri <pedrogodri@gmail.com>
2023-03-22 23:04:37 -0300

    feat: rota pra listar e serviço para listar

--- produtos/src/main/java/br/com/api/produtos/controle/ProdutoControle.java
+++ produtos/src/main/java/br/com/api/produtos/controle/ProdutoControle.java
@@ -1,11 +1,24 @@
 package br.com.api.produtos.controle;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.api.produtos.modelo.ProdutoModelo;
+import br.com.api.produtos.servico.ProdutoServico;
 
 @RestController
 public class ProdutoControle {
     
+    @Autowired
+    private ProdutoServico produtoServico;
+
+    @GetMapping("/listar")
+    public Iterable<ProdutoModelo> listar() {
+        return produtoServico.listar();
+    }
+
+
     @GetMapping("/")
     public String Rota() {{
         return "API de produtos funcionando!";
--- produtos/src/main/java/br/com/api/produtos/servico/ProdutoServico.java
+++ produtos/src/main/java/br/com/api/produtos/servico/ProdutoServico.java
@@ -1,5 +1,18 @@
 package br.com.api.produtos.servico;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.api.produtos.modelo.ProdutoModelo;
+import br.com.api.produtos.repositorio.IProdutoRepositorio;
+
+@Service
 public class ProdutoServico {
-    
+    @Autowired
+    private IProdutoRepositorio produtoRepositorio;
+
+    // Método para listar todos os produtos;
+    public Iterable<ProdutoModelo> listar() {
+        return produtoRepositorio.findAll();
+    }
 }
