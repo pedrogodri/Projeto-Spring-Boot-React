634642eee6d12a2417bdd47f01b28dd26f260767
Pedro Henrique GOdri <pedrogodri@gmail.com>
2023-03-23 20:50:32 -0300

    commit

--- produtos/src/main/java/br/com/api/produtos/servico/ProdutoServico.java
+++ produtos/src/main/java/br/com/api/produtos/servico/ProdutoServico.java
@@ -1,13 +1,11 @@
 package br.com.api.produtos.servico;
 
-import org.apache.catalina.connector.Response;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.ResponseEntity;
+
 import org.springframework.stereotype.Service;
 import br.com.api.produtos.modelo.ProdutoModelo;
-import br.com.api.produtos.modelo.RespostaModelo;
+
 import br.com.api.produtos.repositorio.IProdutoRepositorio;
 
 @Service
@@ -16,24 +14,8 @@ public class ProdutoServico {
     @Autowired
     private IProdutoRepositorio produtoRepositorio;
 
-    @Autowired
-    private RespostaModelo respostaModelo;
-
     // Método para listar todos os produtos;
     public Iterable<ProdutoModelo> listar() {
         return produtoRepositorio.findAll();
-    }
-
-    // Método para cadstrar produtos
-    public ResponseEntity<?> cadastrar(ProdutoModelo produtoModelo) {
-        if(produtoModelo.getNome().equals("")) {
-            respostaModelo.setMensagem("O nome do produto é obrigatório");
-            return new ResponseEntity<RespostaModelo>(respostaModelo, HttpStatus.BAD_REQUEST);
-        } else if(produtoModelo.getMarca().equals("")) {
-            respostaModelo.setMensagem("O nome da marca é obrigatório");
-            return new ResponseEntity<RespostaModelo>(respostaModelo, HttpStatus.BAD_REQUEST);
-        } else {
-            return new ResponseEntity<ProdutoModelo>(produtoRepositorio.save(produtoModelo), HttpStatus.CREATED);
-        }
     }
 }
