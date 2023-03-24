43360209fdeb4ed1b7524b356873c20eb5f8514a
Pedro Henrique GOdri <pedrogodri@gmail.com>
2023-03-23 22:19:46 -0300

    feat: Estrutura do formulário

--- front-produtos/src/Formulario.js
+++ front-produtos/src/Formulario.js
@@ -1,6 +1,14 @@
 function Formulario() {
     return(
-        <h1>Formulário</h1>
+        <form>
+            <input type='text' placeholder='Nome' />
+            <input type='text' placeholder='Marca' />
+
+            <input type='button' value='Cadastrar' />
+            <input type='button' value='Alterar' />
+            <input type='button' value='Remover' />
+            <input type='button' value='Cancelar' />
+        </form>
     )
 }
 
