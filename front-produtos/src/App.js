72dffb99d61f12542777affb5a198c375a7f7ff6
Pedro Henrique GOdri <pedrogodri@gmail.com>
2023-03-25 13:48:48 -0300

    feat: Obtendo os produtos

--- front-produtos/src/App.js
+++ front-produtos/src/App.js
@@ -1,14 +1,26 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Formulario from './Formulario';
 import Tabela from './Tabela';
 
 function App() {
 
+  // UseState
   const [btnCadastrar, setBtnCadastrar] = useState(true);
+  const [produtos, setProdutos] = useState([]);
+
+  // UseEffect
+  useEffect(() => {
+    fetch("http://localhost:8080/listar")
+    .then(retorno => retorno.json())
+    .then(retornoConvertido => setProdutos(retornoConvertido));
+  }, []);
+
+  // Retorno
 
   return (
     <div>
+      <p>{JSON.stringify(produtos)}</p>
       <Formulario botao={btnCadastrar} />
       <Tabela />
     </div>
