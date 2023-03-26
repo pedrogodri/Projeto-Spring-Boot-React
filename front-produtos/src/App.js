af61b1e24ea0a216624ddd7a8a58c056af4ff7d5
Pedro Henrique GOdri <pedrogodri@gmail.com>
2023-03-26 00:10:15 -0300

    Cadastrar produto (parte 1)

--- front-produtos/src/App.js
+++ front-produtos/src/App.js
@@ -29,11 +29,26 @@ function App() {
     setObjProduto({...objProduto, [e.target.name]:e.target.value});
   }
 
+  // Cadastrar produto
+  const cadastrar = () => {
+    fetch('http://localhost:8080/cadastrar', {
+      method: 'post',
+      body: JSON.stringify(objProduto),
+      headers: {
+        'Content-type':'application/json',
+        'Accept':'application/json'
+      }
+    })
+    .then(retorno => retorno.json())
+    .then(retornoConvertido => {
+      console.log(retornoConvertido);
+    })
+  }
+
   // Retorno
   return (
     <div>
-      <div>{JSON.stringify(objProduto)}</div>
-      <Formulario botao={btnCadastrar} eventoTeclado={aoDigitar} />
+      <Formulario botao={btnCadastrar} eventoTeclado={aoDigitar} cadastrar={cadastrar} />
       <Tabela vetor={produtos} />
     </div>
   );
